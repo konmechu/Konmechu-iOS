1afbc22cf2fa7d70f6601cc06bbba8007cb113c9
encoreJeong <zzaibean@gmail.com>
2023-10-25 15:48:09 +0900

    Feat : 영양통계화면, 뷰컨트롤러 연결

---
+++ konmechu/NutritionStatisticsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NutritionStatisticsViewController.swift
+//  konmechu
+//
+//  Created by 정재연 on 10/25/23.
+//
+
+import UIKit
+
+class NutritionStatisticsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var smartlensBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUILayer()
+    }
+    
+    //MARK: - initial UI setting func
+    
+    func setUILayer() {
+        smartlensBtn.layer.cornerRadius = smartlensBtn.bounds.width / 2
+    }
+    
+    //MARK: - btn acction func
+    
+    
+    @IBAction func smartlensBtnDidTap(_ sender: Any) {
+        
+    }
+    
+}
