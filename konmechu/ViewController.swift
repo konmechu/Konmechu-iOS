e4586d746b92cb97c6b9d12a3b2dbc19c60e965b
encoreJeong <zzaibean@gmail.com>
2023-10-25 14:27:54 +0900

    Initial Commit

---
+++ konmechu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  konmechu
+//
+//  Created by 정재연 on 10/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
