f483a4ee4db82a714cc7e69e55a76aa67a993c6a
encoreJeong <zzaibean@gmail.com>
2023-10-25 16:49:41 +0900

    Design : custom tab bar design implemented

--- konmechu/TapBarController.swift
+++ konmechu/TapBarController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class TapBarController: UITabBarController {
         
-    let csView = UIView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupMiddleButton()
+    }
     
-    let tabItem1 = UIButton(type: .system)
-    let tabItem2 = UIButton(type: .system)
+    func setupMiddleButton() {
+        let menuButton = UIButton(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
+                var menuButtonFrame = menuButton.frame
+                menuButtonFrame.origin.y = view.bounds.height - menuButtonFrame.height - 50
+                menuButtonFrame.origin.x = view.bounds.width/2 - menuButtonFrame.size.width/2
+                menuButton.frame = menuButtonFrame
+
+                menuButton.backgroundColor = #colorLiteral(red: 0.3568627451, green: 0.6549019608, blue: 1, alpha: 1)
+                menuButton.layer.cornerRadius = menuButtonFrame.height/2
+                view.addSubview(menuButton)
+
+                menuButton.setImage(UIImage(named: "camera"), for: .normal)
+                menuButton.addTarget(self, action: #selector(menuButtonAction(sender:)), for: .touchUpInside)
+
+                view.layoutIfNeeded()
+    }
     
-    
+    @objc private func menuButtonAction(sender: UIButton) {
+        
+    }
 
 }
