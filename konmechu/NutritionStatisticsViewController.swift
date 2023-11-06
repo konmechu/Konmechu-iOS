feaeb0dda5a11583a2435d60932251190ebbfa3e
encoreJeong <zzaibean@gmail.com>
2023-11-06 15:39:54 +0900

    Feat : Calendar 보기 버트 구현

--- konmechu/NutritionStatisticsViewController.swift
+++ konmechu/NutritionStatisticsViewController.swift
@@ -6,23 +6,67 @@
 //
 
 import UIKit
+import FSCalendar
 
-class NutritionStatisticsViewController: UIViewController {
+class NutritionStatisticsViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
     
-        
+    @IBOutlet weak var dayIdxBtn: UIButton!
+    
+    @IBOutlet weak var FSCalendarView: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setCalendar()
         setUILayer()
+
     }
     
     //MARK: - initial UI setting func
     
     func setUILayer() {
+        
+    }
+    
+    //MARK: - calendar initial setting
+    func setCalendar() {
+        FSCalendarView.delegate = self
+        FSCalendarView.dataSource = self
+        
+        FSCalendarView.locale = Locale(identifier: "ko_KR")
+        
+        FSCalendarView.appearance.headerDateFormat = "YYYY년 MM월"
+        
+        FSCalendarView.appearance.headerTitleAlignment = .center
 
+        FSCalendarView.appearance.headerMinimumDissolvedAlpha = 0.0
+        
+        FSCalendarView.headerHeight = 45
+        
+        FSCalendarView.appearance.headerTitleFont = UIFont(name: "NotoSansKR-Medium", size: 16)
+        FSCalendarView.appearance.headerTitleColor = .white
+
+        FSCalendarView.appearance.weekdayFont = UIFont.boldSystemFont(ofSize: 14)
+        FSCalendarView.appearance.weekdayTextColor = .white
+        
+        FSCalendarView.backgroundColor = FSCalendarView.backgroundColor?.withAlphaComponent(0.2)
+        
+        FSCalendarView.layer.cornerRadius = 30
+        FSCalendarView.clipsToBounds = true
+        
+        FSCalendarView.isHidden = true
+        
     }
     
     //MARK: - btn acction func
+    
+    
+    @IBAction func dayIdxButtonDidTap(_ sender: Any) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.FSCalendarView.isHidden = !self.FSCalendarView.isHidden
+            self.FSCalendarView.alpha = self.FSCalendarView.isHidden ? 0 : 1
+        })
+    }
     
     
 }
